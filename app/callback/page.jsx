695f1b7a7bb67898e48801e3ce30f80481ea127a
NeqTarine <pub@thomas-lacroix.fr>
2024-06-11 19:50:42 +0200

    d Veuillez saisir le message de validation pour vos modifications. Les lignes

--- app/callback/page.jsx
+++
@@ -1,9 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const CallbackComponent = dynamic(() => import('/components/CallbackComponent'), { ssr: false });
-
-const CallbackPage = () => {
-  return <CallbackComponent />;
-};
-
-export default CallbackPage;
